6179bf9c1ff6d589a1b6d41309ffcdd0e343daa3
nikiandonov23 <51458478+nikiandonov23@users.noreply.github.com>
2024-07-28 20:46:41 +0300

    update

--- Regular Expressions - Lab/03. SoftUni Bar Income/Program.cs
+++ Regular Expressions - Lab/03. SoftUni Bar Income/Program.cs
@@ -1,2 +1,65 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//  custumer name
+//  product
+//  count
+//  price
+
+
+using System.Text.RegularExpressions;
+
+List<Product> allProducts = new List<Product>();
+
+string pattern = @"%(?<name>[A-Z][a-z]+)%.*?<(?<product>\w+)>.*?\|(?<count>\d+)\|(?<price>\d+(\.\d+)?)\$";
+
+string input = "";
+while ((input = Console.ReadLine()) != "end of shift")
+{
+
+    Product currentProduct = new Product();
+
+
+    foreach (Match match in Regex.Matches(input, pattern))
+    {
+       
+        
+            currentProduct.Name = match.Groups["name"].Value;
+            currentProduct.Productt = match.Groups["product"].Value;
+            currentProduct.Count = int.Parse(match.Groups["count"].Value);
+            currentProduct.Price = double.Parse(match.Groups["price"].Value);
+
+            allProducts.Add(currentProduct);
+        
+
+
+    }
+
+}
+
+foreach (var product in allProducts)
+{
+    Console.WriteLine($"{product.Name}: {product.Productt} - {product.totalPrice():f2}");
+}
+
+double finalPrice = 0;
+foreach (var product in allProducts)
+{
+    finalPrice += product.totalPrice();
+}
+
+Console.WriteLine($"Total income: {finalPrice:f2}");
+
+
+class Product
+{
+    public string Name { get; set; }
+    public string Productt { get; set; }
+    public int Count { get; set; }
+    public double Price { get; set; }
+
+    public double totalPrice()
+    {
+        return Price * Count;
+    }
+
+}
