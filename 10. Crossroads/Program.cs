097a255455dd3403e0ff13dda9dd71f5ec82ed91
nikiandonov23 <51458478+nikiandonov23@users.noreply.github.com>
2024-09-19 13:02:02 +0300

    update

--- 10. Crossroads/Program.cs
+++ 10. Crossroads/Program.cs
@@ -1,32 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int originalDuration = int.Parse(Console.ReadLine());  //vremeto v koeto moje da trugne kola
+int greenLight = int.Parse(Console.ReadLine());  //vremeto v koeto moje da trugne kola
 int freeWindow=int.Parse(Console.ReadLine());   //vremeto v koeto ve4e trugnala kola moje da se iztegli bezopastno
 
 Queue<string> allCars=new Queue<string>();
 int carsPassed = 0;
+bool weHaveACrash = false;
 
 string input = "";
 while ((input = Console.ReadLine()) != "END")
 {
+    if (weHaveACrash)
+    {
+        break;
+    }
+    
     if (input=="green")
     {
-        int duration = originalDuration;
-        while (allCars.Count>0&& duration>0)
+        int remainingTime = greenLight;
+
+        while (remainingTime>0&&allCars.Count>0)
         {
-            if (allCars.Peek().Length>duration)   // imame katastrofa
+            string currentCar=allCars.Dequeue();
+            remainingTime -= currentCar.Length;
+
+            if (remainingTime<0&&Math.Abs(remainingTime)>freeWindow)
             {
-                int remainingTime = duration + freeWindow;
-                Console.WriteLine($"CRASH");
-                Console.WriteLine($"{allCars.Peek()} was hit at {allCars.Peek()[remainingTime]}.");
+                int partWhereCrashOccured = remainingTime + freeWindow;
+                Console.WriteLine("A crash happened!");
+                Console.WriteLine($"{currentCar} was hit at {currentCar[currentCar.Length+partWhereCrashOccured]}.");
+            
+                weHaveACrash=true;
+                break;
             }
 
             carsPassed++;
-            duration-=allCars.Peek().Length;
-            allCars.Dequeue();
-            
         }
-      
     }
     else
     {
@@ -35,7 +44,9 @@ while ((input = Console.ReadLine()) != "END")
     }
 }
 
-if (carsPassed>0)
+
+
+if (weHaveACrash==false)
 {
     Console.WriteLine("Everyone is safe.");
     Console.WriteLine($"{carsPassed} total cars passed the crossroads.");
