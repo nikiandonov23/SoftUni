1b602f8cf32b029e53bc6d4b64a80fcd01fcba9c
nikiandonov23 <51458478+nikiandonov23@users.noreply.github.com>
2024-09-25 02:47:30 +0300

    update

---
+++ Exercise Multidimensional Arrays/07. Knight Game OPIT 2/Program.cs
@@ -0,0 +1,112 @@
+﻿
+using System.Numerics;
+
+int n = int.Parse(Console.ReadLine());
+char[,] matrix = new char[n,n];
+matrix = readMethod(n);
+
+int[][] allMovements = new[]   //vsi4ki vuzmojni dvijeniq na konq po duskata
+{
+    new int[] { 2, 1 },
+    new int[] { 2, -1 },
+    new int[] { -2, 1 },
+    new int[] { -2, -1 },
+    new int[] { 1, 2 },
+    new int[] { 1, -2 },
+    new int[] { -1, 2 },
+    new int[] { -1, -2 }
+
+};
+
+
+int removedKnights = 0;
+
+bool hasConflicts = true;
+while (hasConflicts)
+{
+    int maxConflicts = 0;
+    int maxConflictRow = 0;
+    int maxConflictCol = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (matrix[i,j]=='K')
+            {
+                int currentConflicts = mostConflictKnight(matrix, i, j);
+                if (maxConflicts<currentConflicts)
+                {
+                    maxConflicts=currentConflicts;
+                    maxConflictRow=i;
+                    maxConflictCol = j;
+                   
+                }
+
+              
+            }
+        }
+    }
+    if (maxConflicts == 0)
+    {
+        hasConflicts = false;
+    }
+    else
+    {
+        matrix[maxConflictRow, maxConflictCol] = '0';
+        removedKnights++;
+    }
+}
+
+Console.WriteLine(removedKnights);
+
+
+
+
+
+
+
+
+
+int mostConflictKnight(char[,]matrix ,int row,int col)
+{
+    int conflictCount = 0;
+    foreach (var move in allMovements)
+    {
+        int nextRow = row + move[0];
+        if (nextRow<0||nextRow>=matrix.GetLength(0))
+        {
+            continue;
+        }
+
+        int nextCol = col + move[1];
+        if (nextCol<0||nextCol>=matrix.GetLength(1))
+        {
+            continue;
+        }
+
+        if (matrix[nextRow,nextCol]=='K')
+        {
+            conflictCount++;
+        }
+    }
+
+    return conflictCount;
+
+}
+char[,] readMethod(int n)
+{
+    char[,] matrix2 = new char[n,n];
+    for (int i = 0; i < n; i++)
+    {
+        string input=Console.ReadLine();
+
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = input[j];
+        }
+    }
+
+    return matrix;
+}
+
+
---
+++ Exercise Multidimensional Arrays/08. Bombs/Program.cs
@@ -0,0 +1,123 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int n =int.Parse(Console.ReadLine());
+
+int[,] matrix = readSquareMatrix(n);
+int[,] bombCoordinates = readBombCoordinates();
+
+for (int i = 0; i < bombCoordinates.GetLength(0); i++)
+{
+    int bombRow=bombCoordinates[i,0];
+    int bombCol=bombCoordinates[i,1];
+
+
+    explodeBomb(matrix, bombRow, bombCol);    //vikam metoda eto ti matricata eto ti current row current col na bombata
+    
+
+}
+
+int aliveCellsCount = 0;
+int aliveCellsSum = 0;
+
+Print(matrix, aliveCellsCount, aliveCellsSum);
+
+
+
+static void Print(int[,] matrix,int aliveCellsCount,int aliveCellsSum)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] > 0)
+            {
+                aliveCellsCount++;
+                aliveCellsSum += matrix[i, j];
+            }
+        }
+    }
+    Console.WriteLine($"Alive cells: {aliveCellsCount}");
+    Console.WriteLine($"Sum: {aliveCellsSum}");
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j > 0)
+            {
+                Console.Write(' ');
+            }
+            Console.Write(matrix[i, j]);
+        }
+
+        Console.WriteLine();
+    }
+}
+
+static void explodeBomb(int[,]matrix , int row,int col)
+{
+    if (matrix[row, col] <= 0) 
+    {
+        return; // AKO IMAME MURTVA KLETKA NA BOMBATA NE  MOJE DA GRUMNE
+    }
+
+    int bombPower = matrix[row, col];
+
+    int rowIterStart = Math.Max(row - 1,0);  //vmesto if proverka .Ako row<0 zapochva ot 0
+    int rowIterEnd = Math.Min(row + 1,matrix.GetLength(0)-1);  //vmesto if proverka. Ako col>=matrix.Lenght
+
+    int colIterStart = Math.Max(col - 1, 0);  
+    int colIterEnd = Math.Min(col + 1, matrix.GetLength(1)-1);
+
+    for (int i = rowIterStart; i <= rowIterEnd; i++)
+    {
+
+        for (int j = colIterStart; j <= colIterEnd; j++)
+        {
+            if (matrix[i,j]>0)
+            {
+                matrix[i, j] -= bombPower;
+                
+            }
+           
+        }
+    }
+
+    matrix[row, col] = 0;
+}
+
+int[,] readBombCoordinates()
+{
+    string[] data = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+    int[,] bombCoordinates = new int[data.Length, 2];
+    for (int i = 0; i < bombCoordinates.GetLength(0); i++)
+    {
+        int[] coordinates = data[i].Split(",").Select(int.Parse).ToArray();
+
+        for (int j = 0; j < bombCoordinates.GetLength(1); j++)
+        {
+            bombCoordinates[i,j]=coordinates[j];
+
+        }
+    }
+
+    return bombCoordinates;
+}
+
+int[,] readSquareMatrix(int n)
+{
+    int[,] matrix = new int[n, n];
+
+    for (int i = 0; i < n; i++)
+    {
+        int[] data=Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = data[j];
+
+        }
+    }
+    return matrix;
+}
+
