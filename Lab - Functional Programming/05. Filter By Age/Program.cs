f2f39fc76b8d0f178b2cd85610f7fbe40596ce94
nikiandonov23 <51458478+nikiandonov23@users.noreply.github.com>
2024-10-07 18:31:02 +0300

    update

--- Lab - Functional Programming/05. Filter By Age/Program.cs
+++ Lab - Functional Programming/05. Filter By Age/Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿    using System.Runtime.InteropServices;
+
+    int n = int.Parse(Console.ReadLine());
+    List<Person> allPersons=new List<Person>();
+    for (int i = 0; i < n; i++)
+    {
+        string[] tockens=Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+        string name = tockens[0];
+        int age=int.Parse(tockens[1]);
+
+        Person currentPerson=new Person();
+        currentPerson.Name=name;
+        currentPerson.Age=age;
+        allPersons.Add(currentPerson);
+        
+    }
+    string filterInput=Console.ReadLine();
+    int ageFilter = int.Parse(Console.ReadLine());
+    string format=Console.ReadLine();
+
+
+    Func<Person, bool> filterFunction=null;
+    switch (filterInput)
+    {
+        case"older":
+            filterFunction = x => x.Age >= ageFilter;
+            break;
+
+
+        case"younger":
+            filterFunction = x => x.Age < ageFilter;
+            break;
+        default:
+            break;
+
+
+    }
+
+    allPersons = FilterPeople(allPersons, filterFunction);
+    List<Person> FilterPeople(List<Person> allPersons, Func<Person, bool> filter)
+    {
+        return allPersons.Where(filter).ToList();
+    }
+
+
+
+    Func<Person, string> formatterFunc = null;
+    switch (format)
+    {
+        case"name":
+            formatterFunc = x => x.Name;
+            break;
+
+        case"age":
+            formatterFunc=x=>x.Age.ToString();
+            break;
+
+        case"name age":
+            formatterFunc = x => $"{x.Name} - {x.Age}";
+            break;
+    }
+
+    PrintPeople(allPersons,formatterFunc);
+    void PrintPeople(List<Person> allPersons, Func<Person, string> formatter)
+    {
+        foreach (var person in allPersons)
+        {
+            Console.WriteLine(formatter(person));
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
---
+++ Lab - Functional Programming/demo/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n = int.Parse(Console.ReadLine());
+List<Person> allPersons = new List<Person>();
+for (int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+    string name = input[0];
+    int age = int.Parse(input[1]);
+
+    Person currentPerson = new Person();
+    currentPerson.Name = name;
+    currentPerson.Age = age;
+    allPersons.Add(currentPerson);
+}
+string condition = Console.ReadLine();
+int threshold = int.Parse(Console.ReadLine());
+string format = Console.ReadLine();
+
+void List<Person> filterFunc(string condition, int threshold)
+{
+    if (condition == "younger")
+    {
+        allPersons = allPersons.Where(x => x.Age < threshold).ToList();
+    }
+
+    else if (condition == "older")
+    {
+        allPersons = allPersons.Where(x => x.Age >= threshold).ToList();
+    }
+
+    
+}
+
+
+
+
+
+
+
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+
+
+}
