185145a3480898bfed7b04512487b86bb985f4d6
nikiandonov23 <51458478+nikiandonov23@users.noreply.github.com>
2024-10-15 20:09:50 +0300

    update

---
+++ Exam Preparation CLASS/0000001. Offroad Challenge/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Stack<int> fuelQuantity = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+
+Queue<int> extraFuel = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+Queue<int> altitudes = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+int altitudeCount = 0;
+int maxAltitude = altitudes.Count;
+
+while (altitudes.Count > 0 && fuelQuantity.Count > 0)
+{
+    int currentFuel = fuelQuantity.Peek();
+    int currentExtra = extraFuel.Peek();
+    int currentNeed = altitudes.Peek();
+
+    if (currentFuel - currentExtra >= currentNeed)
+    {
+        fuelQuantity.Pop();
+        extraFuel.Dequeue();
+        altitudes.Dequeue();
+        altitudeCount++;
+        Console.WriteLine($"John has reached: Altitude {altitudeCount}");
+    }
+    else
+    {
+        Console.WriteLine($"John did not reach: Altitude {altitudeCount + 1}");
+        break;
+    }
+}
+int[] reachedAltitudes = Enumerable.Range(1, altitudeCount).ToArray();
+List<string> array = new List<string>();
+
+foreach (var number in reachedAltitudes)
+{
+    array.Add($"Altitude {number}");
+}
+
+if (altitudeCount <= 0)
+{
+    Console.WriteLine("John failed to reach the top.");
+    Console.WriteLine("John didn't reach any altitude.");
+}
+
+else if (altitudeCount != maxAltitude)
+{
+    Console.WriteLine("John failed to reach the top.");
+    Console.WriteLine($"Reached altitudes: {string.Join(", ", array)}");
+
+}
+else
+{
+    Console.WriteLine("John has reached all the altitudes and managed to reach the top!");
+}
---
+++ Exam Preparation CLASS/0000002. Fishing Competition/Program.cs
@@ -0,0 +1,125 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int n = int.Parse(Console.ReadLine());
+char[,] matrix = new char[n, n];
+
+
+int startRow = -1;
+int startCol = -1;
+
+int currentRow = -1;
+int currentCol = -1;
+for (int i = 0; i < n; i++)
+{
+
+    string input = Console.ReadLine();
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = input[j];
+        if (input[j] == 'S')
+        {
+            startRow = i;
+            startCol = j;
+            currentRow = i;
+            currentCol = j;
+        }
+    }
+}
+
+bool fallIntoAWhirpool = false;
+int totalFish = 0;
+string command = "";
+matrix[startRow, startCol] = '-';
+while ((command = Console.ReadLine()) != "collect the nets")
+{
+
+    switch (command)
+    {
+        case "up":
+            currentRow--;
+            if (currentRow < 0)
+            {
+                currentRow = n - 1;
+            }
+            break;
+
+
+        case "down":
+            currentRow++;
+            if (currentRow >= n)
+            {
+                currentRow = 0;
+            }
+            break;
+
+
+        case "left":
+            currentCol--;
+            if (currentCol < 0)
+            {
+                currentCol = n - 1;
+            }
+            break;
+
+
+        case "right":
+            currentCol++;
+            if (currentCol >= n)
+            {
+                currentCol = 0;
+            }
+            break;
+    }
+
+
+    if (char.IsDigit(matrix[currentRow, currentCol]))
+    {
+        totalFish += matrix[currentRow, currentCol] - '0'; //•	If you move to a fish passage, you collect the amount equal to the digit there, the passage disappears and should be replaced by '-'.
+        matrix[currentRow, currentCol] = '-';
+    }
+    else if (matrix[currentRow, currentCol] == 'W')
+
+    {
+        totalFish = 0;    //program should end
+        Console.WriteLine($"You fell into a whirlpool! The ship sank and you lost the fish you caught. Last coordinates of the ship: [{currentRow},{currentCol}]");
+        return;
+    }
+
+
+
+}
+
+if (!fallIntoAWhirpool)   //ako ne e padal v peralnqta
+{
+    if (totalFish >= 20)
+    {
+        Console.WriteLine("Success! You managed to reach the quota!");
+    }
+    else
+    {
+        Console.WriteLine($"You didn't catch enough fish and didn't reach the quota! You need {20 - totalFish} tons of fish more.");
+
+    }
+}
+
+if (totalFish > 0)
+{
+    Console.WriteLine($"Amount of fish caught: {totalFish} tons.");
+}
+
+if (!fallIntoAWhirpool)
+{
+    matrix[currentRow, currentCol] = 'S';
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write(matrix[i, j]);
+        }
+
+        Console.WriteLine();
+    }
+}
+
+
+
