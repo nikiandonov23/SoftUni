16fc542c46fc0edc0141665e457a6a758203fc86
nikiandonov23 <51458478+nikiandonov23@users.noreply.github.com>
2024-10-17 01:30:27 +0300

    update

---
+++ Exam Preparation CLASS/00000000000002. Blind Man's Buff/Program.cs
@@ -0,0 +1,106 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] dimentions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+int rows = dimentions[0];
+int cols = dimentions[1];
+char[,] matrix = new char[rows, cols];
+
+int currentRow = -1;
+int currentCol = -1;
+
+int startRow = -1;
+int startCol = -1;
+for (int i = 0; i < rows; i++)
+{
+    char[] data = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+    for (int j = 0; j < cols; j++)
+    {
+        matrix[i, j] = data[j];
+        if (data[j] == 'B')
+        {
+            currentRow = i;
+            currentCol = j;
+
+            startRow = i;
+            startCol = j;
+
+        }
+    }
+}
+
+
+
+
+int moves = 0;
+int opponentsTouched = 0;
+string command = "";
+matrix[startRow, startCol] = '-';
+
+while (((command = Console.ReadLine()) != "Finish") && opponentsTouched < 3)
+{
+    int previousRow = currentRow;
+    int previousCol = currentCol;
+    switch (command)
+    {
+        case "up":
+            currentRow--;
+            break;
+
+        case "down":
+            currentRow++;
+            break;
+
+        case "left":
+            currentCol--;
+            break;
+
+        case "right":
+            currentCol++;
+            break;
+    }
+
+    if ((currentRow < 0 || currentRow >= rows || currentCol < 0 || currentCol >= cols) || (matrix[currentRow, currentCol] == 'O'))
+    {
+        currentRow = previousRow;
+        currentCol = previousCol;
+        continue;
+    }
+
+    if (matrix[currentRow,currentCol]=='-')
+    {
+        moves++;
+        continue;
+    }
+    else if (matrix[currentRow,currentCol]=='P')
+    {
+        opponentsTouched++;
+        matrix[currentRow, currentCol] = '-';
+        moves++;
+    }
+
+    matrix[currentRow, currentCol] = 'B';
+}
+
+Console.WriteLine("Game over!");
+Console.WriteLine($"Touched opponents: {opponentsTouched} Moves made: {moves}");
+
+
+
+
+
+
+
+
+void Print(char[,] matrix)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
---
+++ Exam Preparation CLASS/0000000000001. Apocalypse Preparation/Program.cs
@@ -0,0 +1,98 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Security.Cryptography.X509Certificates;
+
+Queue<int> textiles=new Queue<int>(Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+Stack<int> medicaments=new Stack<int>(Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+Dictionary<string, int> healingItems = new Dictionary<string, int>()
+{
+    {"Patch",0},  //30
+    {"Bandage",0}, //40
+    {"MedKit",0}  //100
+};
+
+while (textiles.Count>0&&medicaments.Count>0)
+{
+ 
+
+    int currentTextile = textiles.Peek();
+    int currentMedicament = medicaments.Peek();
+    int sum = currentTextile + currentMedicament;
+
+    if (sum == 30)
+    {
+        healingItems["Patch"] += 1;
+        textiles.Dequeue();
+        medicaments.Pop();
+    }
+    else if (sum == 40)
+    {
+        healingItems["Bandage"] += 1;
+        textiles.Dequeue();
+        medicaments.Pop();
+    }
+    else if (sum == 100)
+    {
+        healingItems["MedKit"] += 1;
+        textiles.Dequeue();
+        medicaments.Pop();
+    }
+    else if (sum > 100)
+    {
+        healingItems["MedKit"] += 1;
+        int leftover = sum - 100;
+        textiles.Dequeue();
+        medicaments.Pop();
+
+        
+        if (medicaments.Count > 0)
+        {
+            medicaments.Push(medicaments.Pop() + leftover);
+        }
+        else
+        {
+            medicaments.Push(leftover); 
+        }
+    }
+    else
+    {
+        textiles.Dequeue();
+        medicaments.Push(medicaments.Pop() + 10); 
+    }
+
+}
+
+if (medicaments.Count == 0 && textiles.Count == 0)
+{
+    Console.WriteLine("Textiles and medicaments are both empty.");
+}
+else if (medicaments.Count == 0)
+{
+    Console.WriteLine("Medicaments are empty.");
+}
+else if (textiles.Count == 0)
+{
+    Console.WriteLine("Textiles are empty.");
+}
+
+
+if (healingItems.Any(x => x.Value > 0))
+{
+    foreach (var item in healingItems.Where(x => x.Value > 0)
+                 .OrderByDescending(x => x.Value)
+                 .ThenBy(x => x.Key))
+    {
+        Console.WriteLine($"{item.Key} - {item.Value}");
+    }
+}
+
+if (medicaments.Count > 0)
+{
+    Console.WriteLine($"Medicaments left: {string.Join(", ", medicaments)}");
+}
+
+if (textiles.Count > 0)
+{
+    Console.WriteLine($"Textiles left: {string.Join(", ", textiles)}");
+}
