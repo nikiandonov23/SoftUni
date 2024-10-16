a535776f3e6a5a33985acb90a7f3bbaef7d64822
nikiandonov23 <51458478+nikiandonov23@users.noreply.github.com>
2024-10-16 16:26:13 +0300

    update

---
+++ Exam Preparation CLASS/0000000002. Mouse In The Kitchen/Program.cs
@@ -0,0 +1,155 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Numerics;
+
+int[] dimentions=Console.ReadLine().Split(",",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+int rows = dimentions[0];
+int cols=dimentions[1];
+
+char[,] matrix = new char[rows, cols];
+
+int totalCheese = 0;
+
+int startRow = -1;
+int startCol = -1;
+
+int currentRow = -1;
+int currentCol = -1;
+for (int i = 0; i < rows; i++)
+{
+    string input=Console.ReadLine();
+    for (int j = 0; j < cols; j++)
+    {
+        matrix[i, j] = input[j];
+        if (input[j]=='M')
+        {
+            currentRow=i; 
+            currentCol=j;
+
+            startRow = i;
+            startCol = j;
+        }
+
+        if (input[j]=='C')
+        {
+            totalCheese++;
+        }
+    }
+}
+
+
+bool trap = false;
+bool outsideBounderies = false;
+
+string command = "";
+while ((command = Console.ReadLine()) != "danger")
+{
+    matrix[startRow, startCol] = '*';
+    int previousRow=currentRow;
+    int previousCol=currentCol;
+    switch (command)
+    {
+        case "up":
+            currentRow--;
+            break;
+
+
+        case "down":
+            currentRow++;
+            break;
+
+
+        case "left":
+            currentCol--;
+            break;
+
+
+        case "right":   
+            currentCol++;
+            break;
+
+    }
+
+    if (currentRow<0||currentRow>=rows||currentCol<0||currentCol>=cols)
+    {
+        outsideBounderies = true;
+        currentRow = previousRow;
+        currentCol = previousCol;
+        matrix[currentRow, currentCol] = 'M';
+        Console.WriteLine("No more cheese for tonight!");
+        break;
+    }
+
+    if (matrix[currentRow,currentCol]=='C')
+    {
+        totalCheese -= 1;
+        matrix[currentRow, currentCol] = '*';
+        if (totalCheese==0)
+        {
+            matrix[currentRow, currentCol] = 'M';
+            Console.WriteLine("Happy mouse! All the cheese is eaten, good night!");
+            break;
+        }
+    }
+
+    if (matrix[currentRow, currentCol] == 'T')
+    {
+        trap=true;
+        matrix[currentRow, currentCol] = 'M';
+        Console.WriteLine("Mouse is trapped!");
+        break;
+    }
+
+    if (matrix[currentRow, currentCol] == '@')
+    {
+        currentRow = previousRow;
+        currentCol = previousCol;
+        continue;
+    }
+
+}
+
+
+
+if (totalCheese>0&&trap==false&& outsideBounderies==false)
+{
+    matrix[currentRow, currentCol] = 'M';
+    Console.WriteLine("Mouse will come back later!");
+}
+Print(matrix);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void Print(char[,] matrix)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            Console.Write(matrix[i,j]);
+        }
+
+        Console.WriteLine();
+    }
+}
---
+++ Exam Preparation CLASS/000000001. Temple of Doom/Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Queue<int> tools = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+Stack<int> substances = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+List<int> challanges = new List<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+
+while (tools.Count > 0 && substances.Count > 0)
+{
+    int currentTool = tools.Peek();
+    int currentSubstance = substances.Peek();
+
+    if (challanges.Contains(currentTool * currentSubstance))
+    {
+        tools.Dequeue();
+        substances.Pop();
+        challanges.Remove(currentTool * currentSubstance);
+
+    }
+    else if (!challanges.Contains(currentTool * currentSubstance))
+    {
+        currentTool += 1;
+        tools.Dequeue();
+        tools.Enqueue(currentTool);
+
+
+
+        currentSubstance -= 1;
+        if (currentSubstance <= 0)
+        {
+            substances.Pop();
+        }
+        else
+        {
+            substances.Pop();
+            substances.Push(currentSubstance);
+        }
+    }
+
+
+
+    if (substances.Count <= 0 && challanges.Count > 0)
+    {
+        Console.WriteLine("Harry is lost in the temple. Oblivion awaits him.");
+        break;
+    }
+    if (challanges.Count <= 0)
+    {
+        Console.WriteLine("Harry found an ostracon, which is dated to the 6th century BCE.");
+    }
+
+}
+
+if (tools.Count>0)
+{
+    Console.WriteLine($"Tools: {string.Join(", ",tools)}");
+}
+
+if (substances.Count>0)
+{
+    Console.WriteLine($"Substances: {string.Join(", ",substances)}");
+}
+
+if (challanges.Count>0)
+{
+    Console.WriteLine($"Challenges: {string.Join(", ",challanges)}");
+}
+
+
