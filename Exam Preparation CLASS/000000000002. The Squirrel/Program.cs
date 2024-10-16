ef50207e217360b06d8c6bae4ee49eaf33391b01
nikiandonov23 <51458478+nikiandonov23@users.noreply.github.com>
2024-10-16 20:14:16 +0300

    update

---
+++ Exam Preparation CLASS/000000000002. The Squirrel/Program.cs
@@ -0,0 +1,116 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Text;
+
+int n = int.Parse(Console.ReadLine());
+List<string>tockens=Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries).ToList();
+
+Queue<string>commands=new Queue<string>();
+foreach (var element in tockens)
+{
+    commands.Enqueue(element);
+}
+
+
+
+int startRow = -1;
+int startCol = -1;
+
+int currentRow = -1;
+int currentCol = -1;
+
+char[,] matrix = new char[n, n];
+for (int i = 0; i < n; i++)
+{
+
+    string data = Console.ReadLine();
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = data[j];
+        if (data[j] == 's')
+        {
+            startRow = i;
+            startCol = j;
+            currentRow = i;
+            currentCol = j;
+        }
+    }
+}
+
+
+
+int hazelNuts = 0;
+bool trap = false;
+bool movesOutTheBounderies = false;
+
+while (hazelNuts < 3 && !trap && !movesOutTheBounderies && commands.Count > 0)
+{
+    string currentCommand = commands.Dequeue();
+
+    switch (currentCommand)
+    {
+        case "up":
+            currentRow--;
+            break;
+
+
+        case "down":
+            currentRow++;
+            break;
+
+
+        case "left":
+            currentCol--;
+            break;
+
+
+        case "right":
+            currentCol++;
+            break;
+    }
+
+    if (currentRow < 0 || currentRow >= n || currentCol < 0 || currentCol >= n)
+    {
+        movesOutTheBounderies = true;
+        Console.WriteLine("The squirrel is out of the field.");
+        break;
+    }
+
+    if (matrix[currentRow, currentCol] == 't')
+    {
+        trap = true;
+        Console.WriteLine("Unfortunately, the squirrel stepped on a trap...");
+        break;
+    }
+
+    if (matrix[currentRow, currentCol] == 'h')
+    {
+        hazelNuts++;
+        matrix[currentRow, currentCol] = '*';
+        if (hazelNuts >= 3)
+        {
+            matrix[currentRow, currentCol] = 's';
+            break;
+        }
+    }
+
+    if (matrix[currentRow, currentCol] == '*')
+    {
+        continue;
+    }
+}
+
+if (hazelNuts < 3 && !movesOutTheBounderies && !trap)
+{
+    Console.WriteLine("There are more hazelnuts to collect.");
+}
+else if (hazelNuts >= 3 && !movesOutTheBounderies && !trap)
+{
+    Console.WriteLine("Good job! You have collected all hazelnuts!");
+}
+
+
+Console.WriteLine($"Hazelnuts collected: {hazelNuts}");
+
+
---
+++ Exam Preparation CLASS/00000000001. Rubber Duck Debuggers/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.ComponentModel.Design;
+
+Queue<int> times=new Queue<int>(Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+Stack<int> tasks=new Stack<int>(Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+Dictionary<string, int> DuckType = new Dictionary<string, int>()
+{
+    {"Darth Vader Ducky", 0 },
+    {"Thor Ducky",0},
+    {"Big Blue Rubber Ducky",0},
+    {"Small Yellow Rubber Ducky",0}
+};
+
+
+while (times.Count>0&&tasks.Count>0)
+{
+    int currentTime = times.Dequeue();
+    int currentTask = tasks.Pop();
+
+    if (currentTime*currentTask>=0&&currentTime*currentTask<=240)
+    {
+        if (currentTime*currentTask>=0&&currentTime*currentTask<=60)
+        {
+            DuckType["Darth Vader Ducky"] += 1;
+        }
+
+        else if (currentTime * currentTask >= 61&& currentTime * currentTask <=120)
+        {
+            DuckType["Thor Ducky"] += 1;
+
+        }
+        else if (currentTime * currentTask >= 121 && currentTime * currentTask <= 180)
+        {
+            DuckType["Big Blue Rubber Ducky"] += 1;
+
+        }
+        else
+        {
+            DuckType["Small Yellow Rubber Ducky"] += 1;
+
+        }
+    }
+    else
+    {
+        currentTask -= 2;
+        tasks.Push(currentTask);
+
+        times.Enqueue(currentTime);
+    }
+}
+
+Console.WriteLine("Congratulations, all tasks have been completed! Rubber ducks rewarded:");
+foreach (var duck in DuckType)
+{
+    Console.WriteLine($"{duck.Key}: {duck.Value}");
+}
