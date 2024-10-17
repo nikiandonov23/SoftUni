a032b7b4d3681284f4b3f0e700b6d459c9909b73
nikiandonov23 <51458478+nikiandonov23@users.noreply.github.com>
2024-10-17 15:49:45 +0300

    update

---
+++ Exam Preparation CLASS 2/01. Climb The Peaks/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Stack<int> foods = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+Queue<int> stamina = new Queue<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+
+
+Dictionary<int, string> peeks = new Dictionary<int, string>()
+{
+    {80,"Vihren"},   //80
+    {90,"Kutelo"},   //90
+    {100,"Banski Suhodol"},   //100 
+    {60,"Polezhan"},  //60
+    {70,"Kamenitza"},  //70
+
+
+};
+int peeksInitialCount=peeks.Count;
+List<string>conqueredPeeks=new List<string>();
+
+while (foods.Count > 0 && stamina.Count > 0&&peeks.Count>0)
+{
+
+    int currentFood= foods.Peek();
+    int currentStamina = stamina.Peek();
+
+    int currentSum=currentFood+currentStamina;
+
+
+    var peekToCompare = peeks.First();
+    if (peekToCompare.Key<=currentSum)
+    {
+        conqueredPeeks.Add(peekToCompare.Value);
+        peeks.Remove(peeks.First().Key);
+
+        foods.Pop();
+        stamina.Dequeue();
+        
+    }
+    else if (peekToCompare.Key>currentSum)
+    {
+        foods.Pop();
+        stamina.Dequeue();
+    }
+
+}
+
+if (peeks.Count!=0)
+{
+    Console.WriteLine("Alex failed! He has to organize his journey better next time -> @PIRINWINS");
+}
+else if (peeks.Count==0)
+{
+    Console.WriteLine("Alex did it! He climbed all top five Pirin peaks in one week -> @FIVEinAWEEK");
+    
+}
+
+if (conqueredPeeks.Count!=0)
+{
+    Console.WriteLine("Conquered peaks:");
+    foreach (var peek in conqueredPeeks)
+    {
+        Console.WriteLine(peek.Trim());
+    }
+}
---
+++ Exam Preparation CLASS 2/02. Navy Battle/Program.cs
@@ -0,0 +1,123 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int n = int.Parse(Console.ReadLine());
+
+char[,] matrix = new char[n, n];
+
+
+int startRow = -1;
+int startCol = -1;
+
+int currentRow = -1;
+int currentCol = -1;
+for (int i = 0; i < n; i++)
+{
+    string input = Console.ReadLine();
+
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = input[j];
+        if (input[j] == 'S')
+        {
+            startRow = i; startCol=j; currentRow = i; currentCol = j;
+        }
+    }
+}
+
+
+matrix[startRow, startCol] = '-';
+int directHits = 3;
+int battleCruisers = 3;
+while (directHits>0&&battleCruisers>0)
+{
+    string command=Console.ReadLine();
+    switch (command)
+    {
+        case "up":
+            currentRow--;
+            break;
+
+        case "down":
+            currentRow++;
+            break;
+
+        case "left":
+            currentCol--;
+            break;
+        
+        case "right":
+            currentCol++;
+            break;
+
+    }
+
+    if (matrix[currentRow,currentCol]=='*')
+    {
+        directHits--;
+        if (directHits==0)
+        {
+            matrix[currentRow, currentCol] = 'S';
+            Console.WriteLine($"Mission failed, U-9 disappeared! Last known coordinates [{currentRow}, {currentCol}]!");
+            break;
+        }
+        else
+        {
+            matrix[currentRow, currentCol] = '-';
+        }
+        
+        continue;
+    }
+
+    if (matrix[currentRow,currentCol]=='C')
+    {
+        battleCruisers--;
+        if (battleCruisers == 0)
+        {
+            matrix[currentRow, currentCol] = 'S';
+            Console.WriteLine("Mission accomplished, U-9 has destroyed all battle cruisers of the enemy!");
+            break;
+        }
+        else
+        {
+            matrix[currentRow, currentCol] = '-';
+        }
+        
+    }
+}
+
+Print(matrix);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void Print(char[,] matrix)
+{
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write(matrix[i, j]);
+        }
+
+        Console.WriteLine();
+    }
+}
