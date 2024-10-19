1c252067dec06254c9dec9dca9968d145b274bf2
nikiandonov23 <51458478+nikiandonov23@users.noreply.github.com>
2024-10-19 22:26:31 +0300

    update

---
+++ Exam Preparation CLASS 2/000000002. The Battle of The Five Armies/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int armor = int.Parse(Console.ReadLine());
+
+int n = int.Parse(Console.ReadLine()); // n ROWS
+
+char[][] jaggedArray = new char[n][];
+
+for (int i = 0; i < jaggedArray.Length; i++)
+{
+    string input = Console.ReadLine();
+    for (int j = 0; j < input.Length; j++)
+    {
+        jaggedArray[i] = input.ToCharArray();
+    }
+}
+
+Console.WriteLine();
--- Exam Preparation CLASS 2/00001. Meal Plan/Program.cs
+++ Exam Preparation CLASS 2/00001. Meal Plan/Program.cs
@@ -1,13 +1,12 @@
 ﻿Queue<string> meals = new Queue<string>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries));
-
 Stack<int> calories = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
 
-Dictionary<string, int> list = new Dictionary<string, int>()
+Dictionary<string, int> mealCalories = new Dictionary<string, int>()
 {
-    {"salad",350},
-    {"soup",490},
-    {"pasta",680},
-    {"steak",790},
+    {"salad", 350},
+    {"soup", 490},
+    {"pasta", 680},
+    {"steak", 790},
 };
 
 int mealsEaten = 0;
@@ -15,37 +14,48 @@ int mealsEaten = 0;
 while (meals.Count > 0 && calories.Count > 0)
 {
     int currentDayCalories = calories.Pop(); 
+
     while (currentDayCalories > 0 && meals.Count > 0)
     {
-        int currentMealCalories = list[meals.Dequeue()]; 
+        string currentMeal = meals.Dequeue();
+        int currentMealCalories = mealCalories[currentMeal];
         mealsEaten++;
 
         if (currentDayCalories >= currentMealCalories)
         {
-            currentDayCalories -= currentMealCalories; 
+            currentDayCalories -= currentMealCalories;
         }
         else
         {
-            
             int remainingMealCalories = currentMealCalories - currentDayCalories;
             currentDayCalories = 0; 
 
             if (calories.Count > 0)
             {
-                int nextDayCalories = calories.Pop(); 
-                nextDayCalories -= remainingMealCalories; 
-                calories.Push(nextDayCalories); 
+                int nextDayCalories = calories.Pop();
+                nextDayCalories -= remainingMealCalories;
+
+                if (nextDayCalories > 0)
+                {
+                    calories.Push(nextDayCalories);
+                }
             }
         }
     }
+
+    if (currentDayCalories > 0)
+    {
+        calories.Push(currentDayCalories);
+    }
 }
+
 
 if (meals.Count == 0)
 {
     Console.WriteLine($"John had {mealsEaten} meals.");
     if (calories.Count > 0)
     {
-        Console.WriteLine($"For the next few days, he can eat {string.Join(", ", calories.Reverse())} calories.");
+        Console.WriteLine($"For the next few days, he can eat {string.Join(", ", calories)} calories.");
     }
 }
 else
